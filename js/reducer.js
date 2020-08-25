4cad4c0c09410db27267f60a053e2c6cde9844a9
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-08-25 20:59:42 +0000

    Automatically backed up by Learn

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,20 @@
 // add code snippets from README
+let state = {count: 0}
+ 
+function reducer(state, action){
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return {count: state.count + 1};
+    default:
+      return state;
+  }
+}
+
+function dispatch(action){
+  state = reducer(state, action);
+}
+
+function render(){
+  let container = document.getElementById('container');
+  container.textContent = state.count;
+}
